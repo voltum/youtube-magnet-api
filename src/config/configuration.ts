6ad4036fc9ebbaf8d5042dc62b9dfb8ecd7f72e1
Сарвар Муталов <mutalov02@mail.ru>
2024-11-07 02:20:40 +0500

    fix: configuration variable fixed

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,7 @@
 import { Logger } from "@nestjs/common";
 
 export default () => {
-  const { PORT, REDIS_HOST, REDIS_PORT, USER_DATA_DIR, YOUTUBE_API_KEY } = process.env;
+  const { PORT, REDIS_HOST, REDIS_PORT, USER_DATA_DIR, YOUTUBE_API_KEY, MONGODB_STRING } = process.env;
 
   return {
     port: parseInt(PORT, 10) || 3000,
@@ -12,5 +12,6 @@ export default () => {
     getRedisHost: () => REDIS_HOST || 'localhost',
     getRedisPort: () => Number(REDIS_PORT) || 6379,
     getYoutubeApiKey: () => YOUTUBE_API_KEY || 'AIzaSyBquyODQDQl7mf82awWwWZzAUqYBkTRMgQ'
+    getMongoDBString: () => MONGODB_STRING,
   }
 };
